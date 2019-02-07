302bbe44639b4061efb767e2efab715ccaeeec34
Benjamin Denizart <benjamin.denizart@free.fr>
2019-02-07 09:07:58 +0100

    master Add Exercice5 + Unit testing

---
+++ exercice/exercice5.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+list($N, $Q) = array_shift($args);
+
+for ($i = 0; $i < $Q; $i++) {
+    list($L, $R) = $args[$i];
+}
+
+// Write an action using echo().
+echo('0');
